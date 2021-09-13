22efee7f58bdcb1d4b89f4b3285d9819b1c128d3
MuradAbuArqoub <muradaboarkob2000@gmail.com>
2021-09-13 18:26:46 +0300

    i wanna cry so damn bad

---
+++ src/component/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render(){
+    return(
+      <p>auther: murad</p>
+    )
+  }
+}
+export default Footer
---
+++ src/component/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Beast from "./Beast";
+import Data from "./Data.json";
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                {
+                  
+                }
+            </div>
+        )
+    }
+}
+export default Main
